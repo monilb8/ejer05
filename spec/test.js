37b6881be85055fb8fb15341ffb5127ef3518034
java <lbmoni8@gmail.com>
2017-07-05 18:02:54 +0200

    Primer commit

---
+++ spec/test.js
@@ -0,0 +1,14 @@
+var miModulo = require('../index.js');
+
+describe("A suite is just a function", function() {
+  var resultado;
+
+  it("and so is a spec", function() {
+    resultado = miModulo.sumar(4, 5);
+
+    expect(resultado).toBe(9);
+  });
+});
+    
+
+
